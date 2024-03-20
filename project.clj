6384479cea2c91ca137bd04be597fd3faf2258c0
Libero Passador Neto <libero.neto@vitta.me>
2024-03-19 21:45:24 -0300

    first commit

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject code-challenge "0.1.0-SNAPSHOT"
+(defproject financial-market "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
